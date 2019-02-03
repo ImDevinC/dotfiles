7b852e4d51de2578d1fc886a8a3b6b5c70ec281d
Devin Collins <me@imdevinc.com>
2019-02-03 15:49:06 -0800

    Added links

--- createlinks.sh
+++ createlinks.sh
@@ -3,18 +3,18 @@ git submodule init
 git submodule update
  
 command -v i3 > /dev/null 2>&1 && {
-  ln -sf ./i3/config "$HOME/.config/i3/config"
-  ln -sf ./polybar "$HOME/.config/polybar"
+  ln -sf "$(PWD)/i3/config" "$HOME/.config/i3/config"
+  ln -sf "${PWD}/polybar" "$HOME/.config/polybar"
 }
 
 command -v zsh > /dev/null 2>&1 && {
-  ln -sf ./zsh/.oh-my-zsh "$HOME/.oh-my-zsh"
-  ln -sf ./zsh/.zshrc "$HOME/.zshrc"
+  ln -sf "${PWD}/zsh/.oh-my-zsh" "$HOME/.oh-my-zsh"
+  ln -sf "${PWD}/zsh/.zshrc" "$HOME/.zshrc"
 }
 
 if [ -d "$HOME/Library/Application Support/iTerm2/DynamicProfiles" ]; then
-  ln -sf ./iTerm/profile.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles/profile.json"
+  ln -sf "${PWD}/iTerm/profile.json" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/profile.json"
 fi
 
 mkdir -p "$HOME/.vim/colors" > /dev/null 2>&1
-cp vim/miniml/colors/miniml.vim "$HOME/.vim/colors/miniml.vim"
+cp "${PWD}/vim/miniml/colors/miniml.vim" "$HOME/.vim/colors/miniml.vim"
