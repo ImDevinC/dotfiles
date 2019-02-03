2379ced353ee08796af413bf3cd5715aac8a4a86
Devin Collins <me@imdevinc.com>
2019-02-03 15:37:16 -0800

    Added vim theme

--- createlinks.sh
+++ createlinks.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+git submodule init
+git submodule update
+ 
 command -v i3 > /dev/null 2>&1 && {
-  ln -sf ~/.dotfiles/i3/config ~/.config/i3/config
-  ln -sf ~/.dotfiles/polybar ~/.config/polybar
+  ln -sf ./i3/config "$HOME/.config/i3/config"
+  ln -sf ./polybar "$HOME/.config/polybar"
 }
 
 command -v zsh > /dev/null 2>&1 && {
-  git submodule init
-  git submodule update
-  ln -sf ~/.dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
-  ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+  ln -sf ./zsh/.oh-my-zsh "$HOME/.oh-my-zsh"
+  ln -sf ./zsh/.zshrc "$HOME/.zshrc"
 }
 
-if [ -d "~/Library/Application Support/iTerm2/DynamicProfiles" ]; then
-  ln -sf ~/.dotfiles/iTerm/profile.json "~/Library/Application Support/iTerm2/DynamicProfiles/profile.json"
+if [ -d "$HOME/Library/Application Support/iTerm2/DynamicProfiles" ]; then
+  ln -sf ./iTerm/profile.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles/profile.json"
 fi
+
+mkdir -p "$HOME/.vim/colors" > /dev/null 2>&1
+cp vim/miniml/colors/miniml.vim "$HOME/.vim/colors/miniml.vim"
